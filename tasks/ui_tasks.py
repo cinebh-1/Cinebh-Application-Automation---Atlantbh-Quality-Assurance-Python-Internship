6db8e1f5253938e14b89b4cd33b428bcfe2b6353
Mario Nizic <marionizic@msn.com>
2026-02-02 20:49:52 +0100

    Added suggested changes as per mentor's new approach

--- tasks/ui_tasks.py
+++ tasks/ui_tasks.py
@@ -5,77 +5,101 @@ from tests.conftest import page_manager
 from utilities.email_getter import EmailExtractor
 from utilities.logging import log_info
 load_dotenv()
+
 class Tasks:
+    _page_manager = None
+
+    @classmethod
+    def set_page_manager(cls, page_manager):
+        cls._page_manager = page_manager
+
+    @staticmethod
     @allure.step("Navigate to Sign In Page")
-    def navigate_to_sign_in_modal(page_manager):
+    def navigate_to_sign_in_modal():
         log_info("Navigate to Sign In Page")
-        page_manager.home_page.navigate()
-        page_manager.home_page.click_on_sign_in_btn()
+        Tasks._page_manager.home_page.navigate()
+        Tasks._page_manager.home_page.click_on_sign_in_btn()
+
+    @staticmethod
     @allure.step("Register and Verify via Gmail")
-    def complete_registration(page_manager, user):
+    def complete_registration(user):
         app_password = os.getenv("GMAIL_APP_PASSWORD")
-        page_manager.signin_page.click_on_sign_up_link()
+        Tasks._page_manager.signin_page.click_on_sign_up_link()
         log_info(f"Filling registration form for: {user['email']}")
-        page_manager.signup_page.enter_email(user["email"])
-        page_manager.signup_page.enter_password(user["password"])
-        page_manager.signup_page.enter_confirm_password(user["confirm_password"])
-        page_manager.signup_page.click_on_sign_up_button()
+        Tasks._page_manager.signup_page.enter_email(user["email"])
+        Tasks._page_manager.signup_page.enter_password(user["password"])
+        Tasks._page_manager.signup_page.enter_confirm_password(user["confirm_password"])
+        Tasks._page_manager.signup_page.click_on_sign_up_button()
         log_info("Fetching verification code from Gmail...")
         verification_code = EmailExtractor.extract_verification_code(user["email"], app_password)
         log_info(f"Extracted code: {verification_code}")
         log_info("Entering verification code and finalizing registration")
-        page_manager.signup_page.fill_verification_code(verification_code)
-        page_manager.signup_page.click_on_continue_button()
+        Tasks._page_manager.signup_page.fill_verification_code(verification_code)
+        Tasks._page_manager.signup_page.click_on_continue_button()
         log_info("Success message checked")
-        page_manager.signup_page.check_success_message()
+        Tasks._page_manager.signup_page.check_success_message()
+
+    @staticmethod
     @allure.step("Complete Login")
-    def complete_login(page_manager,user):
+    def complete_login(user):
         log_info("Complete Login")
-        page_manager.signin_page.click_on_sign_in_btn()
-        page_manager.signin_page.enter_email(user["email"])
-        page_manager.signin_page.enter_password(user["password"])
-        page_manager.signin_page.click_on_sign_in_btn()
+        Tasks._page_manager.signin_page.click_on_sign_in_btn()
+        Tasks._page_manager.signin_page.enter_email(user["email"])
+        Tasks._page_manager.signin_page.enter_password(user["password"])
+        Tasks._page_manager.signin_page.click_on_sign_in_btn()
+
+    @staticmethod
     @allure.step("Check Logged In User")
-    def check_logged_in_user(page_manager):
+    def check_logged_in_user():
         log_info("Check Username")
-        page_manager.signin_page.check_username_button()
+        Tasks._page_manager.signin_page.check_username_button()
+
+    @staticmethod
     @allure.step("Navigate to Currently Showing Page")
-    def navigate_to_currently_showing_page(page_manager):
+    def navigate_to_currently_showing_page():
         log_info("Navigate to Currently Showing Page")
-        page_manager.home_page.navigate()
-        page_manager.home_page.click_on_currently_showing_menu_item()
+        Tasks._page_manager.home_page.navigate()
+        Tasks._page_manager.home_page.click_on_currently_showing_menu_item()
+
+    @staticmethod
     @allure.step("Search movie, select city,cinema, genre and time")
-    def search_movie_and_verify_selection(page_manager,projection):
+    def search_movie_and_verify_selection(projection):
         log_info(f"Search Movie - {projection['search_term']}")
-        page_manager.currently_showing_page.enter_search_term(projection['search_term'])
+        Tasks._page_manager.currently_showing_page.enter_search_term(projection['search_term'])
         log_info(f"Select City - {projection['city']} ")
-        page_manager.currently_showing_page.select_city(projection['city'])
+        Tasks._page_manager.currently_showing_page.select_city(projection['city'])
         log_info(f"Select Cinema - {projection['cinema']} ")
-        page_manager.currently_showing_page.select_cinema(projection['cinema'])
+        Tasks._page_manager.currently_showing_page.select_cinema(projection['cinema'])
         log_info(f"Select Genre - {projection['genre']} ")
-        page_manager.currently_showing_page.select_genre(projection['genre'])
+        Tasks._page_manager.currently_showing_page.select_genre(projection['genre'])
         log_info(f"Select Time - {projection['time']} ")
-        page_manager.currently_showing_page.select_time(projection['time'])
+        Tasks._page_manager.currently_showing_page.select_time(projection['time'])
         log_info("Select Random Date - date tabs - currently showing page")
-        page_manager.currently_showing_page.select_date()
+        Tasks._page_manager.currently_showing_page.select_date()
+
+    @staticmethod
     @allure.step("Navigate to Upcoming Movies Page")
-    def navigate_to_upcoming_movies_page(page_manager):
+    def navigate_to_upcoming_movies_page():
         log_info("Navigate to Upcoming Movies Page")
-        page_manager.home_page.navigate()
-        page_manager.home_page.click_on_upcoming_movies_menu_item()
+        Tasks._page_manager.home_page.navigate()
+        Tasks._page_manager.home_page.click_on_upcoming_movies_menu_item()
+
+    @staticmethod
     @allure.step("Select city, cinema and genre")
-    def selection_city_cinema_genre(page_manager,projection):
+    def selection_city_cinema_genre(projection):
         log_info(f"Select City - {projection['city']} ")
-        page_manager.upcoming_movies_page.select_city(projection['city'])
+        Tasks._page_manager.upcoming_movies_page.select_city(projection['city'])
         log_info(f"Select Cinema - {projection['cinema']} ")
-        page_manager.upcoming_movies_page.select_cinema(projection['cinema'])
+        Tasks._page_manager.upcoming_movies_page.select_cinema(projection['cinema'])
         log_info(f"Select Genre - {projection['genre']}")
-        page_manager.upcoming_movies_page.select_genre(projection['genre'])
+        Tasks._page_manager.upcoming_movies_page.select_genre(projection['genre'])
+
+    @staticmethod
     @allure.step("Select date range for upcoming movies")
-    def select_date_range_upcoming_movies(page_manager):
+    def select_date_range_upcoming_movies():
         log_info("Select date range for upcoming movies")
-        page_manager.upcoming_movies_page.click_on_data_range_dropdown()
+        Tasks._page_manager.upcoming_movies_page.click_on_data_range_dropdown()
         log_info("Select random start date and end date for upcoming movies")
-        page_manager.upcoming_movies_page.select_random_dates_date_range()
+        Tasks._page_manager.upcoming_movies_page.select_random_dates_date_range()
         log_info("Click on apply button")
-        page_manager.upcoming_movies_page.click_on_apply_button()
+        Tasks._page_manager.upcoming_movies_page.click_on_apply_button()
--- tests/UI_tests/test_smoke.py
+++ tests/UI_tests/test_smoke.py
@@ -6,36 +6,41 @@ from tasks.ui_tasks import Tasks
 from test_data.projection_data import projection
 from utilities.user_data import user
 
-@allure.title("Smoke Test UI Cinebh")
-@allure.description("This is a smoke test for Cinebh Apllication, which tests basic features like register,login, currently showing and upcoming movies")
-@allure.tag("Smoke")
-@allure.severity(allure.severity_level.BLOCKER)
-@allure.label("owner", "Mario Nizic")
 @allure.suite("Smoke Test")
-@allure.link("http://localhost:5173/")
-@allure.feature("Registration,Login, Currently Showing, Upcoming Movies")
-@allure.story("Smoke Test - Basic Features - UI")
-@allure.testcase("CAAI-93")
-@pytest.mark.smoke_ui
-@pytest.mark.order(1)
-def test_smoke(page_manager):
+class TestSmoke:
 
-   Tasks.navigate_to_sign_in_modal(page_manager)
+   @allure.title("Smoke Test UI Cinebh")
+   @allure.description("This is a smoke test for Cinebh Apllication, which tests basic features like register,login, currently showing and upcoming movies")
+   @allure.tag("Smoke")
+   @allure.severity(allure.severity_level.BLOCKER)
+   @allure.label("owner", "Mario Nizic")
+   @allure.suite("Smoke Test")
+   @allure.link("http://localhost:5173/")
+   @allure.feature("Registration,Login, Currently Showing, Upcoming Movies")
+   @allure.story("Smoke Test - Basic Features - UI")
+   @allure.testcase("CAAI-93")
+   @pytest.mark.smoke_ui
+   @pytest.mark.order(1)
+   def test_smoke(self, page_manager):
 
-   Tasks.complete_registration(page_manager,user)
+      Tasks.set_page_manager(page_manager)
 
-   Tasks.complete_login(page_manager,user)
+      Tasks.navigate_to_sign_in_modal()
 
-   Tasks.check_logged_in_user(page_manager)
+      Tasks.complete_registration(user)
 
-   Tasks.navigate_to_currently_showing_page(page_manager)
+      Tasks.complete_login(user)
 
-   Tasks.search_movie_and_verify_selection(page_manager, projection)
+      Tasks.check_logged_in_user()
 
-   Tasks.navigate_to_upcoming_movies_page(page_manager)
+      Tasks.navigate_to_currently_showing_page()
 
-   Tasks.selection_city_cinema_genre(page_manager, projection)
+      Tasks.search_movie_and_verify_selection(projection)
 
-   Tasks.select_date_range_upcoming_movies(page_manager)
+      Tasks.navigate_to_upcoming_movies_page()
 
-   APITasks.delete_user(APITasks,user)
+      Tasks.selection_city_cinema_genre(projection)
+
+      Tasks.select_date_range_upcoming_movies()
+
+      APITasks().delete_user(user)
